164acdf31117066c55432dd0eb7f8829f7ab2b02
Tyson Hester <thester@PetSafe.net>
2014-03-19 14:25:24 -0400

    added detail level to toString and changed unspecified detail level for errors to 0

--- IronLogEntry.js
+++ IronLogEntry.js
@@ -4,71 +4,72 @@ function IronLogEntry(type, message, detailLevel, data, child) {
 		return new IronLogEntry(type, message, data, child);
 	}
 
-    this.data = data === void 0 ? {} : data;
-    this.child = child === void 0 ? void 0 : new IronLogEntry(child);
-    this.detailLevel = detailLevel;
+	this.data = data === void 0 ? {} : data;
+	this.child = child === void 0 ? void 0 : new IronLogEntry(child);
+	this.detailLevel = detailLevel;
 
-    if (type instanceof Error || type instanceof IronLogEntry) {
+	if (type instanceof Error || type instanceof IronLogEntry) {
 		this.message = type.message;
-        if (type instanceof IronLogEntry) {
-            this.type = type.type;
-            this.data = type.data;
-            this.child = type.child;
-        }
-        else {
-            this.type = type.name;
-            this.stack = type.stack.substring(type.stack.indexOf('\n') + 1).replace(/    at /g, "at ");
-            if (type.arguments !== void 0) {
-                this.data.arguments = type.arguments;
-            }
-        }
-        if (message instanceof IronLogEntry) {
-            this.child = message;
-        }
-        else if (message instanceof Error) {
-            this.child = new IronLogEntry(message);
-        }
-        else if (typeof message === "object") {
-            this.data = message
-        }
-    }
-    else {
-        this.type = type;
+		if (type instanceof IronLogEntry) {
+			this.type = type.type;
+			this.data = type.data;
+			this.child = type.child;
+		}
+		else {
+			this.type = type.name;
+			this.stack = type.stack.substring(type.stack.indexOf('\n') + 1).replace(/    at /g, "at ");
+			if (type.arguments !== void 0) {
+				this.data.arguments = type.arguments;
+			}
+		}
+		if (message instanceof IronLogEntry) {
+			this.child = message;
+		}
+		else if (message instanceof Error) {
+			this.child = new IronLogEntry(message);
+		}
+		else if (typeof message === "object") {
+			this.data = message
+		}
+	}
+	else {
+		this.type = type;
 		this.message = message;
-    }
-    if (this.type !== void 0 && this.type.toLowerCase().indexOf('error') > 0 && this.detailLevel === void 0) {
-        this.detailLevel = Number.MAX_VALUE;
-    }
+	}
+	if (this.type !== void 0 && this.type.toLowerCase().indexOf('error') > 0 && this.detailLevel === void 0) {
+		this.detailLevel = 0;
+	}
 }
 
 IronLogEntry.prototype.toString = function (nest) {
-    if (nest === void 0) {
-        nest = 0;
-    }
-    var linePrefix = "";
-    for (var i = 0; i < nest; i++) {
-        linePrefix += "\t";
-    }
-    return linePrefix + this.type + ": " + this.message + "\n" +
-        linePrefix + "Stack: " + ("\n" + this.stack).replace(/\n/g, "\n\t" + linePrefix) + "\n" +
-        linePrefix + "Data: " + JSON.stringify(this.data) + "\n" +
-        linePrefix + "Child: \n" + (
-            this.child !== void 0
-                ? this.child.toString(nest + 1)
-                : linePrefix + "\tundefined"
-        );
+	if (nest === void 0) {
+		nest = 0;
+	}
+	var linePrefix = "";
+	for (var i = 0; i < nest; i++) {
+		linePrefix += "\t";
+	}
+	return linePrefix + this.type + ": " + this.message + "\n" +
+		linePrefix + "Detail Level: " + this.detailLevel + "\n" +
+		linePrefix + "Stack: " + ("\n" + this.stack).replace(/\n/g, "\n\t" + linePrefix) + "\n" +
+		linePrefix + "Data: " + JSON.stringify(this.data) + "\n" +
+		linePrefix + "Child: \n" + (
+		this.child !== void 0
+			? this.child.toString(nest + 1)
+			: linePrefix + "\tundefined"
+		);
 };
 
 IronLogEntry.prototype.fromJSON = function (json) {
-    var obj = JSON.parse(json);
-    this.type = obj.type;
-    this.message = obj.message;
-    if (obj.stack !== void 0) {
-        this.stack = obj.stack;
-    }
-    this.data = obj.data;
-    this.child = obj.child;
-    return this;
+	var obj = JSON.parse(json);
+	this.type = obj.type;
+	this.message = obj.message;
+	if (obj.stack !== void 0) {
+		this.stack = obj.stack;
+	}
+	this.data = obj.data;
+	this.child = obj.child;
+	return this;
 };
 
 module.exports = IronLogEntry;
