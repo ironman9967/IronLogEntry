757aab2f0fee5446eef6f5b6475c3089838c5195
Tyson Hester <tyson@decodingideas.com>
2014-11-26 18:50:55 -0500

    new version

--- IronLogEntry.js
+++ IronLogEntry.js
@@ -1,65 +1,79 @@
 
-function IronLogEntry(type, message, detailLevel, data, child) {
+var _ = require('lodash');
+var uuid = require('uuid');
+
+function processOptions(opts) {
+	if (opts === void 0) {
+		opts = {};
+	}
+	return {
+		type: opts.type === void 0 ? IronLogEntry.types.error : opts.type,
+		detailLevel: opts.detailLevel === void 0 ? 0 : opts.detailLevel,
+		data: {},
+		message: "",
+		types: opts.types === void 0 ? IronLogEntry.types : opts.types
+	};
+}
+
+function setProp(prop, entry, opts, instance) {
+	instance[prop] = entry[prop] !== void 0 ? entry[prop] : opts[prop];
+}
+
+function IronLogEntry(entry, opts) {
 	if ((this instanceof IronLogEntry) === false) {
-		return new IronLogEntry(type, message, data, child);
+		return new IronLogEntry(entry, opts);
 	}
 
-    if (data !== void 0 && (data instanceof Error || data instanceof IronLogEntry)) {
-        child = data;
-    }
-    else if (data !== void 0) {
-        this.data = data;
-    }
-    else {
-        this.data = {};
-    }
-    if (detailLevel !== void 0 && (detailLevel instanceof Error || detailLevel instanceof IronLogEntry)) {
-        child = detailLevel;
-    }
-    else if (typeof detailLevel === "number") {
-        this.detailLevel = detailLevel;
-    }
-    if (typeof child === "object" && !(child instanceof Error || child instanceof IronLogEntry)) {
-        this.child = new IronLogEntry(child.type, child.message, child.detailLevel, child.data, child.child);
-    }
-    else if (child !== void 0) {
-        this.child = new IronLogEntry(child);
-    }
+	this.id = entry.id === void 0 ? uuid.v4() : entry.id;
+	this.timestamp = entry.timestamp === void 0 ? new Date().getTime() : entry.timestamp;
 
-	if (type instanceof Error || type instanceof IronLogEntry) {
-		this.message = type.message;
-		if (type instanceof IronLogEntry) {
-			this.type = type.type;
-			this.data = type.data;
-			this.child = type.child;
-			this.stack = type.stack;
+	this.opts = processOptions(opts);
+
+	if (entry instanceof Error) {
+		this.message = entry.message;
+		this.type = {
+			type: entry.name,
+			addStack: true
+		};
+		this.stack = entry.stack.substring(entry.stack.indexOf('\n') + 1).replace(/    at /g, "at ");
+		if (entry.arguments !== void 0) {
+			this.data.arguments = entry.arguments;
 		}
 		else {
-			this.type = type.name;
-			this.stack = type.stack.substring(type.stack.indexOf('\n') + 1).replace(/    at /g, "at ");
-			if (type.arguments !== void 0) {
-				this.data.arguments = type.arguments;
-			}
+			this.data = {};
 		}
-		if (message instanceof IronLogEntry) {
-			this.child = message;
-		}
-		else if (message instanceof Error) {
-			this.child = new IronLogEntry(message);
-		}
-		else if (typeof message === "object") {
-			this.data = message
-		}
+		setProp('detailLevel', {}, this.opts, this);
 	}
 	else {
-		this.type = type;
-		this.message = message;
-        this.stack = new Error().stack.replace(/.*\n.*at new IronLogEntry \(.*\)\n/g, "").replace(/.*at/g, "at");
-	}
-	if (this.type !== void 0 && this.type.toLowerCase().indexOf('error') >= 0 && this.detailLevel === void 0) {
-		this.detailLevel = 0;
+		var instance = this;
+		_.each(_.keys(this.opts), function (key) {
+			if (!_.contains([ 'types' ], key)) {
+				setProp(key, entry, instance.opts, instance);
+			}
+		});
+		this.userSuppliedKeys = [];
+		_.each(_.difference(_.keys(entry), _.keys(this.opts)), function (key) {
+			if (!_.contains([ 'id', 'timestamp', 'opts', 'stack', 'child' ], key)) {
+				instance.userSuppliedKeys.push(key);
+				setProp(key, entry, instance.opts, instance);
+			}
+		});
+
+		if (this.type !== void 0 && this.type.addStack !== void 0 && this.type.addStack)  {
+			this.stack = new Error().stack.replace(/.*at/g, "at")
+				.replace(/.*\n.*at new IronLogEntry \(.*\)\n/g, "")
+				.replace(/.*at IronLogEntry.addChild \(.*\)\n/g, "");
+		}
 	}
 }
+
+IronLogEntry.prototype.addChild = function (child) {
+	this.child = new IronLogEntry(child);
+	if (child.child !== void 0) {
+		this.child.addChild(child.child);
+	}
+	return this;
+};
 
 IronLogEntry.prototype.toString = function (nest) {
 	if (nest === void 0) {
@@ -69,23 +83,67 @@ IronLogEntry.prototype.toString = function (nest) {
 	for (var i = 0; i < nest; i++) {
 		linePrefix += "\t";
 	}
-	return linePrefix + this.type + ": " + this.message + "\n" +
+	var type = typeof this.type === "object" ? this.type.type : this.type;
+	var str = linePrefix + type + "(" + this.id + "): " + this.message + "\n" +
+		linePrefix + "Timestamp: " + new Date(this.timestamp) + "\n" +
 		linePrefix + "Detail Level: " + this.detailLevel + "\n" +
-		linePrefix + "Stack: " + ("\n" + this.stack).replace(/\n/g, "\n\t" + linePrefix) + "\n" +
-		linePrefix + "Data: " + JSON.stringify(this.data) + "\n" +
-        (this.child !== void 0
-			? linePrefix + "Child: \n" +this.child.toString(nest + 1)
-			: ""
-		);
+		(this.stack !== void 0 ? linePrefix + "Stack: " + ("\n" + this.stack).replace(/\n/g, "\n\t" + linePrefix) + "\n" : "") +
+		linePrefix + "Data: " + JSON.stringify(this.data) + "\n";
+	var instance = this;
+	_.each(this.userSuppliedKeys, function (key) {
+		var objStr = typeof instance[key] === "object"
+			? JSON.stringify(instance[key])
+			: instance[key].toString();
+		str += linePrefix + key + ": " + objStr + "\n";
+	});
+	str += (this.child !== void 0
+		? linePrefix + "Child: \n" + this.child.toString(nest + 1)
+		: "");
+	return str;
 };
 
 IronLogEntry.prototype.getJSON = function () {
-    return JSON.stringify(this);
+    return JSON.stringify(getCleanObject(this), null, '\t');
 };
 
-IronLogEntry.fromJSON = function (json) {
-    var obj = JSON.parse(json);
-	return new IronLogEntry(obj.type, obj.message, obj.detailLevel, obj.data, obj.child);
+function getCleanObject(ironLogEntry) {
+	var t = _.cloneDeep(ironLogEntry);
+	t.type = typeof ironLogEntry.type === "object" ? ironLogEntry.type.type : ironLogEntry.type;
+	t.opts = void 0;
+	if (t.child !== void 0) {
+		t.child = getCleanObject(t.child);
+	}
+	return t;
+}
+
+IronLogEntry.fromJSON = function (json, opts) {
+	var obj = JSON.parse(json);
+	var entry = new IronLogEntry(obj, opts);
+	if (obj.child !== void 0) {
+		entry.child = IronLogEntry.fromJSON(JSON.stringify(obj.child), opts);
+	}
+	return entry;
+};
+
+IronLogEntry.types = {
+	info: {
+		type: 'Info'
+	},
+	warn: {
+		type: 'Warn'
+	},
+	error: {
+		type: 'Error',
+		addStack: true
+	}
+};
+
+IronLogEntry.createFromOptions = function (opts) {
+	return {
+		create: function (entry) {
+			return new IronLogEntry(entry, opts);
+		}
+	}
 };
 
 module.exports = IronLogEntry;
---
+++ test.js
@@ -0,0 +1,22 @@
+
+var IronLogEntry = require('./IronLogEntry');
+
+var factory = IronLogEntry.createFromOptions({
+    type: IronLogEntry.types.info,
+    detailLevel: 0
+});
+
+var x = factory.create({
+    message: "test message",
+    userInfo: {
+        some: "user data or object"
+    }
+}).addChild(factory.create({
+    message: "child",
+    type: {
+        type: 'Error',
+        addStack: true
+    }
+}).addChild(new Error("grandchild")));
+
+console.log(x.toString());
